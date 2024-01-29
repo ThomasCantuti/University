25e38287027b8d3a715cd11c585025a848a0807e
Thomas <thomas.cantuti@gmail.com>
2024-01-29 12:25:58 +0100

    SO fork execlp pipe

---
+++ SO/C/Funzioni_Librerie.c
@@ -0,0 +1,30 @@
+/*
+
+Funzioni della libreria stdlib.h
+    atexit()
+    exit()
+    on_exit()
+    system()
+
+Funzioni della libreria stdio.h
+    popen(), pclose()
+
+Funzioni della libreria unistd.h
+    _exit()
+    alarm()
+    execl(), execv(), execle(), execlp(), execvp()
+    execve()
+    fork()
+    pause()
+    pipe()
+    sleep()
+
+Funzioni della libreria sys/wait.h
+    wait(), waitpid()
+
+Funzioni della libreria signal.h
+    kill()
+    killpg()
+    signal()
+
+*/
