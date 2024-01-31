2bb83a41ea0e8971a82e107dd29db3bf4c8b9219
Thomas <thomas.cantuti@gmail.com>
2024-01-31 12:11:41 +0100

    fine esame test_eseguiti

--- SO/C/Esami/test_eseguiti.c
+++ SO/C/Esami/test_eseguiti.c
@@ -3,6 +3,7 @@
 #include <unistd.h>
 #include <signal.h>
 #include <fcntl.h>
+#include <string.h>
 #include <sys/wait>
 
 static volatile sig_atomic_t count = 0;
@@ -14,8 +15,8 @@ void handler () {
 
 int main (int argc, char** argv) {
 
-    int pid, p0p1[2], p0p2[2], fd, status;
-    char *filepath, *data;
+    int pid, p0p1[2], p0p2[2], fd, status, nread, nwrite;
+    char *filepath, *nomeRef, *results;
 
     // controllo argomenti
     if (argc != 3) {
@@ -72,14 +73,65 @@ int main (int argc, char** argv) {
         close(p0p2[0]);
 
         while(1) {
-            
+            // leggo nome refertatore da terminale
+            nread = read(p0p1[0], nomeRef, sizeof(nomeRef) - 1);
+            if (nread < 0) {
+                perror("P1: read\n");
+                exit(8);
+            }
+            nomeRef[nread] = '\0';
+
+            pid = fork();
+            if (pid < 0) {
+                perror("P1: fork\n");
+                exit(9);
+            }
+            if (pid == 0) {
+                // codice P2
+
+                close(p0p1[0]);
+
+                close(1);
+                dup(p0p2[1]);
+                close(p0p2[1]);
+
+                execlp("grep", "grep", nomeRef, filepath, NULL);
+                perror("P2: grep\n");
+                exit(10);
+            }
+
+            // attendo terminazione P2
+            wait(&status);
         }
 
+        close(p0p1[0]);
+        close(p0p2[1]);
+
+        while (1) {
+            printf("Inserire refertatore: ");
+            scanf("%s", nomeRef);
+
+            // P0 invia nome a P1
+            nwrite = write(p0p1[1], nomeRef, strlen(nomeRef) + 1);
+            if (nwrite != (int)(strlen(nomeRef) + 1)) {
+                perror("P0: write\n");
+                exit(11);
+            }
+
+            // P0 legge e stampa i risultati ricevuti da P2
+            nread = read(p0p2[0], results, sizeof(results) - 1);
+            if (nread < 0) {
+                perror("P0: read\n");
+                exit(12);
+            }
+            results[nread] = '\0';
+            printf("%s\n", results);
+
+            // incrementonumero di byte ricevuti
+            count += nread;
+        }
 
     }
-
-
-    printf("Numero totale di byte ricevuti: %d\n", count);
 
     return 0;
 }
