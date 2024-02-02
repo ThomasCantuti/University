ba4376523bf14d86bfa1c752a1d2affac0999281
Thomas <thomas.cantuti@gmail.com>
2024-02-02 23:32:51 +0100

    esame trova_video.c

--- SO/Java/Esami/RilevarePollini/RilevatoreAria.java
+++ SO/Java/Esami/RilevarePollini/RilevatoreAria.java
@@ -1,8 +1,8 @@
 package SO.Java.Esami.RilevarePollini;
 
-import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.io.PipedOutputStream;
 import java.util.concurrent.atomic.AtomicBoolean;
 
@@ -16,18 +16,34 @@ public class RilevatoreAria extends Thread {
     }
 
     public void run () {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String line = null;
 
-        try {
+        isRunning.set(true);
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(pos));
 
-        } catch (IOException e) {
-            e.printStackTrace();
+        while (isRunning.get()) {
+            try {
+                bw.write((int)110.0*Math.random() + "");
+                bw.newLine();
+                bw.write(System.currentTimeMillis() / 1000 + "");
+                bw.newLine();
+                bw.flush();
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            try {
+                Thread.sleep(12000);
+                
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
+        
     }
 
     public void fermaRilevatoreAria () {
-
+        isRunning.set(false);
     }
 
 }
