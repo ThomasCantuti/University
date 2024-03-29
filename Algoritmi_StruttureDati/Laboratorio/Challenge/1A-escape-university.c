94a7ee12918b83867137657440201d5b2081dc46
Thomas <thomas.cantuti@gmail.com>
2024-03-29 18:06:00 +0100

    challenge con errori di acquisizione informazioni da file input

--- Algoritmi_StruttureDati/Laboratorio/Challenge/1A-escape-university.c
+++ Algoritmi_StruttureDati/Laboratorio/Challenge/1A-escape-university.c
@@ -1,20 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// idea: provare a creare una typdef con struttura file[key] = [value]
-// con key = id e value = num (senza dim)
-
-FILE *in_file;
-FILE *out_file;
-
-int solve (int n) {
-    
-}
-
 int main () {
+    FILE *in_file, *out_file;
 
     int N, Q;
-    int id, dim, *num;
+    int S, x, id, file[N][S];
+    int ans[Q], req[Q];
 
     in_file = fopen("input.txt", "r");
     out_file = fopen("output.txt", "w");
@@ -24,14 +16,42 @@ int main () {
         return -2;
     }
 
-    fscanf(in_file, "%d %d", &N, &Q);
-    // lettura finche non arriva alla fine del file
-        // lettura finchè non arriva alla fine della riga
-    
-    // soluzione
+    // riempimento di null nell'array di risposta
+    for (int i = 0; i < Q; i ++)
+        ans[i] = 0;
 
-    //fprintf(out_file, "%d", 1);
-    
+    // lettura N Q
+    fscanf(in_file, "%d %d", &N, &Q);
+    // lettura id Sn x0...xSn-1
+    for (int i = 0; i < N; i ++) {
+        fscanf(in_file, "%d %d", &id, &S);
+        file[i][0] = id;
+        file[i][1] = S;
+        for (int j = 2; j < S - 1; j ++)
+            fscanf(in_file, "%d", &file[i][j]);
+    }
+    // lettura ultima riga input -> x che mi fa trovare l'id
+    for (int i = 0; i < Q; i++)
+        fscanf(in_file, "%d", &req[i]);
+
+    for (int i = 0; i < N; i ++) {
+        for (int j = 0; j < file[i][1]; j ++)
+            printf("%d ", file[i][j]);
+        printf("\n");
+    }
+/*
+    // soluzione
+    for (int row = 0; row < Q; row ++) {
+        for (int col = 0; col < file[row][1]; col ++) {
+            if (req[row] == file[row][col])
+                ans[row] = file[row][0];
+        }
+    }
+
+    // scrittura risposta su file output
+    for (int i = 0; i < Q; i ++)
+        fprintf(out_file, "%d ", ans[i]);
+*/
     if (fclose(in_file) != 0 || fclose(out_file) != 0) {
         fprintf(stderr, "I/O Error");
         return -2;
