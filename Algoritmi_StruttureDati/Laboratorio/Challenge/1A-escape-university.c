ebe91bb878881d394fa2521bfe281a3a7243c037
Thomas <thomas.cantuti@gmail.com>
2024-04-10 23:40:58 +0200

    challenge 1A completata

--- Algoritmi_StruttureDati/Laboratorio/Challenge/1A-escape-university.c
+++ Algoritmi_StruttureDati/Laboratorio/Challenge/1A-escape-university.c
@@ -12,7 +12,7 @@ struct pair {
 /**
  * Sottoprocedura di 'merge_sort' per unire due range.  
  */
-void merge(struct pair *arr, int start, int mid, int end) {
+void merge(struct pair *pairs, int start, int mid, int end) {
     int n1 = mid - start + 1;
     int n2 = end - mid;
 
@@ -20,11 +20,11 @@ void merge(struct pair *arr, int start, int mid, int end) {
     struct pair left[n1], right[n2];
 
     for (i=0; i<n1; i++) {
-        left[i] = arr[start+i];
+        left[i] = pairs[start+i];
     }
 
     for (j=0; j<n2; j++) {
-        right[j] = arr[mid+1+j];
+        right[j] = pairs[mid+1+j];
     }
 
     i = j = 0;
@@ -32,12 +32,12 @@ void merge(struct pair *arr, int start, int mid, int end) {
     for (int k=start; k<=end; k++) {
         if (i < n1) {
             if (j < n2) {
-                arr[k] = (left[i].x <= right[j].x) ? left[i++] : right[j++];
+                pairs[k] = (left[i].x <= right[j].x) ? left[i++] : right[j++];
             } else {
-                arr[k] = left[i++];
+                pairs[k] = left[i++];
             }
         } else {
-            arr[k] = right[j++];
+            pairs[k] = right[j++];
         }
     }
 }
@@ -45,13 +45,45 @@ void merge(struct pair *arr, int start, int mid, int end) {
 /**
  * Implementazione di 'merge sort'.
  */
-void merge_sort(int *arr, int start, int end) {
+void merge_sort(struct pair *pairs, int start, int end) {
     if (start < end) {
         int mid = start + (end-start) / 2;
-        merge_sort(arr, start, mid);
-        merge_sort(arr, mid+1, end);
-        merge(arr, start, mid, end);
+        merge_sort(pairs, start, mid);
+        merge_sort(pairs, mid+1, end);
+        merge(pairs, start, mid, end);
     }
+}
+
+/**
+ * Implementazione di 'binary search'.
+ */
+struct pair find (struct pair *pairs, int low ,size_t pairs_size, int target) {
+    if (low > pairs_size)
+        return ;
+    
+    int mid = (low + pairs_size) / 2;
+    if (pairs[mid].x == target)
+        return pairs[mid];
+    if (pairs[mid].x < target)
+        return find (pairs, mid + 1, pairs_size, target);
+    if (pairs[mid].x > target)
+        return find (pairs, low, mid - 1, target);
+}
+
+/**
+* Soluzione del problema implementando l'algoritmo
+* binary search e scrivendo il risultato nel file output
+*/
+void solve (struct pair *pairs, int pairs_size, int query, FILE *out_file) {
+    // uso algoritmo binary search
+    struct pair p = find(pairs, 0, pairs_size, query);
+
+    // se risultato trovato -> lo stampo
+    // altrimenti -> stampo null
+    if (p.x == query)
+        fprintf(out_file, "%d ", p.id);
+    else
+        fprintf(out_file, "NULL ");
 }
 
 
@@ -83,9 +115,6 @@ void escape_university(FILE *in_file, FILE *out_file) {
         fscanf(in_file, "%d", &query);
         solve(pairs, pairs_size, query, out_file);
     }
-        
-
-
 }
 
 int main () {
@@ -99,50 +128,10 @@ int main () {
 
     escape_university(in_file, out_file);
 
-/*
-    int N, Q;
-    int S, x, id, file[N][S];
-    int ans[Q], req[Q];
-
-    // lettura N Q
-    fscanf(in_file, "%d %d", &N, &Q);
-    // lettura id Sn x0...xSn-1
-    for (int i = 0; i < N; i ++) {
-        fscanf(in_file, "%d %d", &id, &S);
-        file[i][0] = id;
-        file[i][1] = S;
-        for (int j = 2; j < S - 1; j ++)
-            fscanf(in_file, "%d", &file[i][j]);
-    }
-    // lettura ultima riga input -> x che mi fa trovare l'id
-    for (int i = 0; i < Q; i++)
-        fscanf(in_file, "%d", &req[i]);
-
-    for (int i = 0; i < N; i ++) {
-        for (int j = 0; j < file[i][1]; j ++)
-            printf("%d ", file[i][j]);
-        printf("\n");
-    }
-    
-
-    // soluzione
-    for (int row = 0; row < Q; row ++) {
-        for (int col = 0; col < file[row][1]; col ++) {
-            if (req[row] == file[row][col])
-                ans[row] = file[row][0];
-        }
-    }
-
-    // scrittura risposta su file output
-    for (int i = 0; i < Q; i ++)
-        fprintf(out_file, "%d ", ans[i]);
-*/
-
-
     if (fclose(in_file) != 0 || fclose(out_file) != 0) {
         fprintf(stderr, "I/O Error");
         return -2;
     }
-    return 0;
 
+    return 0;
 }
