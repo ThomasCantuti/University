64f978693b4c90d63e38d34e5fc2b10efb5930a5
Thomas <thomas.cantuti@gmail.com>
2024-04-22 16:39:45 +0200

    laboratorio 02 es 03 e 04

--- SO/Java/Es_Lab/Lab_01/02_es_ComunicazioneThread/ByteArray/Main.java
+++ SO/Java/Es_Lab/Lab_01/02_es_ComunicazioneThread/ByteArray/Main.java
@@ -2,7 +2,7 @@ import java.io.PipedInputStream;
 import java.io.PipedOutputStream;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+    public static void main (String[] args) throws Exception {
         PipedOutputStream pos = new PipedOutputStream();
         PipedInputStream pis = new PipedInputStream(pos);
     
